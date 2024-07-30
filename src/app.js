162e73d7e080439337f0ff7efc6b951a6816955a
junior mojica <juniormojica26@gmail.com>
2024-07-30 08:47:18 -0500

    importing  providedServiceRouter to app.js file

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import clientRouter from './routes/clients.js';
 import paymentMethodRouter from './routes/paymentMethodRoutes.js';
 import paymentRouter from './routes/paymentRoutes.js';
+import providedServiceRouter from './routes/providedServiceRoute.js';
 const app = express();
 
 // Usa el router de clientes para manejar las rutas que comienzan con /clientes
@@ -11,7 +12,8 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use('/clientes', clientRouter);
 app.use('/paymentmethods', paymentMethodRouter)
-app.use('/payment', paymentRouter)
+app.use('/payments', paymentRouter)
+app.use('/providedservices', providedServiceRouter)
 
 
 
