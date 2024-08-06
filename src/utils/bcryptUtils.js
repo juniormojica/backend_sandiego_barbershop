e79540ff55d8610a9f7fbba47701893e19b3c6c8
junior mojica <juniormojica26@gmail.com>
2024-08-05 22:08:25 -0500

    create bcript fn for compare and hash password

---
+++ src/utils/bcryptUtils.js
@@ -0,0 +1,11 @@
+import bcrypt from "bcrypt";
+export const encriptPassWord = async (password) => {
+  const salt = await bcrypt.genSalt(10)
+  const passwordHashed = await bcrypt.hash(password, salt)
+  console.log('user Password' + passwordHashed);
+
+  return passwordHashed
+}
+export const comparePassword = async (password, passwordToCompare) => {
+  return await bcrypt.compare(password, passwordToCompare)
+}
---
+++ src/utils/checkUsername.js
@@ -0,0 +1,21 @@
+
+import sequelize from "../database/db.js"
+const { User } = sequelize.models
+export const checkUserName = async (username) => {
+  try {
+    const userNameFound = await User.findOne({
+      where: {
+        username: username
+      }
+    })
+    if (!userNameFound) {
+      throw new Error("The username doesnt exist");
+
+    }
+    return userNameFound
+  } catch (error) {
+    console.log(`Error in checkUserName: ${error.message}`);
+    throw error
+  }
+
+}
