6f7eb9a35c05fd87d3c38f3a2b0c50125c28a6d9
junior mojica <juniormojica26@gmail.com>
2024-08-06 13:37:19 -0500

    creating CreateUserHandler fn

--- src/handlers/userHandlers.js
+++ src/handlers/userHandlers.js
@@ -1,4 +1,7 @@
-import { getAllUsers, getUserById } from "../controllers/userController.js"
+import { getAllUsers, getUserById, createUserController } from "../controllers/userController.js"
+import { encriptPassWord } from "../utils/bcryptUtils.js"
+import jwt from 'jsonwebtoken'
+const { SECRET } = process.env
 export const getUsersHandler = async (req, res) => {
   try {
     const users = await getAllUsers()
@@ -29,6 +32,35 @@ export const getUserByIdHandler = async (req, res) => {
 }
 
 
-export const createUserHandler = async () => {
+export const createUserHandler = async (req, res) => {
+  try {
+    let { username, password, email, roles, isActive } = req.body
+    if (!username || !password || !email) {
+      return res.status(400).json({ error: true, message: 'Faltan datos para crear el usuario: Username, Password, o Email' });
+    }
+    const user = {
+      username,
+      email,
+      password: await encriptPassWord(password),
+      roles: roles,
+      isActive
+    }
+    const newUser = await createUserController(user)
 
+    const token = jwt.sign({ id: newUser.id }, SECRET, { expiresIn: 86400 })
+    const responseUser = {
+      id: newUser.id,
+      username: newUser.username,
+      email: newUser.email,
+      roles: newUser.roles,
+      isActive: newUser.isActive
+    };
+
+    res.status(201).json({ error: false, token, user: responseUser })
+
+
+  } catch (error) {
+    res.status(400).json({ error: true, message: error.message })
+
+  }
 }
