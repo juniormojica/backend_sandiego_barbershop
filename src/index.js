d07895b3c4960aca00ddf6289652cddda28a7c35
junior mojica <juniormojica26@gmail.com>
2024-08-06 14:04:24 -0500

    standard config added and formating index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 // index.js
-import 'dotenv/config';
-import app from './app.js';
-import sequelize from './database/db.js';
+import 'dotenv/config'
+import app from './app.js'
+import sequelize from './database/db.js'
 import { injectSeeds } from './seeds/dbSeed.js'
 
-const SERVER_PORT = process.env.SERVER_PORT || 3000;
-
+const SERVER_PORT = process.env.SERVER_PORT || 3000
 
 try {
   await sequelize.sync({ logging: false, alter: true })
   // await injectSeeds()
   app.listen(SERVER_PORT, () => {
-    console.log(`Running on port ${SERVER_PORT}`);
-  });
-
-
+    console.log(`Running on port ${SERVER_PORT}`)
+  })
 } catch (error) {
-  console.log(error);
+  console.log(error)
 }
-
