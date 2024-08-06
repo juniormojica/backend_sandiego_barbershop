884022e0a3e550ce2add8b479b2b22ff21aa8771
junior mojica <juniormojica26@gmail.com>
2024-08-06 13:36:41 -0500

    adding op from sequelize and creating createUserController fn for creating new users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import sequelize from "../database/db.js"
+import { Op } from "sequelize"
 const { User } = sequelize.models
 export const getAllUsers = async () => {
   const users = await User.findAll({ attributes: { exclude: ['password'] } })
@@ -23,4 +24,46 @@ export const getUserById = async (id) => {
     throw Error(error.message)
   }
 
-} 
+}
+
+
+export const createUserController = async (user) => {
+  try {
+    const { username, email, password, roles, isActive } = user
+
+    // Verificar si el correo electrónico ya existe
+    const existingUser = await User.findOne({
+      where: {
+        [Op.or]: [
+          { email },
+          { username }
+        ]
+      }
+    });
+
+    if (existingUser) {
+      if (existingUser.email === email) {
+        throw new Error('El correo electrónico ya está registrado.');
+      }
+      if (existingUser.username === username) {
+        throw new Error('El nombre de usuario ya está registrado.');
+      }
+    }
+
+
+    // Si no existen registros con el correo electrónico o el nombre de usuario, crear uno nuevo
+    const newUser = await User.create({
+      username,
+      password,
+      email,
+      roles,
+      isActive
+    });
+    return newUser;
+
+  } catch (error) {
+    throw new Error(error.message);
+
+  }
+
+}
