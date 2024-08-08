2fcce85a299d66c44d851fe09519f10e5ae0faa6
junior mojica <juniormojica26@gmail.com>
2024-08-08 18:24:13 -0500

    get/id getAll and post roleHandler creation

---
+++ src/handlers/roleHandlers.js
@@ -0,0 +1,40 @@
+import { createRolCtrl, getAllRolesCtrl, getRoleByIdCtrl } from '../controllers/roleControllers.js'
+export const postRole = async (req, res) => {
+  try {
+    const { roleName } = req.body
+    const newRole = await createRolCtrl({ roleName })
+    if (!newRole) {
+      throw new Error('No fue posible crear el rol')
+    }
+    res.status(201).json({ error: false, data: newRole })
+  } catch (error) {
+    res.status(400).json({ error: true, message: error.message })
+  }
+}
+
+export const getAllRoles = async (req, res) => {
+  try {
+    const allRoles = await getAllRolesCtrl()
+    if (!allRoles) {
+      throw new Error('No Existen Roles')
+    }
+    res.status(201).json({ error: false, data: allRoles })
+  } catch (error) {
+    res.status(400).json({ error: true, message: error.message })
+  }
+}
+
+export const getRoleById = async (req, res) => {
+  try {
+    const { id } = req.params
+    console.log(id)
+
+    const role = await getRoleByIdCtrl(id)
+    if (!role) {
+      throw new Error(`No se encontro el role con el id ${id}`)
+    }
+    res.status(201).json({ error: false, data: role })
+  } catch (error) {
+    res.status(400).json({ error: true, message: error.message })
+  }
+}
