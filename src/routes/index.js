6e279239b4c5b76271e805923225cdb7447e5d25
junior mojica <juniormojica26@gmail.com>
2024-08-08 12:30:41 -0500

    change convention name for router routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import autRouter from './authRoutes.js'
-import clientRouter from './clients.js'
+import clientRouter from './clientRoutes.js'
 import paymentMethodRouter from './paymentMethodRoutes.js'
 import paymentRouter from './paymentRoutes.js'
-import providedServiceRouter from './providedServiceRoute.js'
-import userRouter from './userRoute.js'
+import providedServiceRouter from './providedServiceRoutes.js'
+import userRouter from './userRoutes.js'
 
 export {
   autRouter, clientRouter, paymentMethodRouter, paymentRouter, providedServiceRouter, userRouter
