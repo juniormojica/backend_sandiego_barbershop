3fe14d83e5531322613eb644f6cbeb4f60059065
junior mojica <juniormojica26@gmail.com>
2024-08-10 13:59:33 -0500

    add /providedservice/:id route

--- src/routes/providedServiceRoutes.js
+++ src/routes/providedServiceRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { getProvidedHandler, postProvidedHandler } from '../handlers/providedServiceHandlers.js'
+import { getProvidedHandler, postProvidedHandler, getProvidedByIdHandler } from '../handlers/providedServiceHandlers.js'
 const providedServiceRouter = Router()
 
 providedServiceRouter
   .get('/', getProvidedHandler)
+  .get('/:id', getProvidedByIdHandler)
   .post('/', postProvidedHandler)
 
 export default providedServiceRouter
